183102c0fa9c6a5b9c098a5d9dfabb165204ba15
steven lee <stevenlee@stevens-mac.local>
2016-06-12 21:25:02 +0800

    finish creating the graph and the information, there is a need to update the information that is provided and fix the alignment of the information shown

--- Cryptanalysis/InputViewController.swift
+++ Cryptanalysis/InputViewController.swift
@@ -50,3 +50,5 @@ class InputViewController: UIViewController, UITextViewDelegate {
 
 }
 
+
+
--- Cryptanalysis/StatisticalViewController.swift
+++ Cryptanalysis/StatisticalViewController.swift
@@ -7,17 +7,91 @@
 //
 
 import UIKit
+import Charts
 
 class StatisticalViewController: UIViewController {
 
+    
+    @IBOutlet weak var chartView: BarChartView!
+    
+    @IBOutlet weak var informationTextView: UITextView!
+    @IBOutlet weak var characterFrequencyLength: UISlider!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        chartView.noDataText = "There is no data being provided"
+        chartView.descriptionText = ""
+        chartView.animate(xAxisDuration: 2, yAxisDuration: 2)
+        chartView.doubleTapToZoomEnabled = false
+        generateChart(Int(characterFrequencyLength.value))
+        setCharts(getXAxisLabel(), values: getXAxisData(getXAxisLabel()))
+        informationTextView.text = getStaticalInformation()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        
+        generateChart(Int(characterFrequencyLength.value))
+        setCharts(getXAxisLabel(), values: getXAxisData(getXAxisLabel()))
+        informationTextView.text = getStaticalInformation()
+    }
+    
+    @IBAction func characterFrequencyDidChange(sender: UISlider) {
+        
+        sender.setValue(Float(lround(Double(characterFrequencyLength.value))), animated: true)
+        
+        if(Int(sender.value) <= trimmedText.characters.count)
+        {
+            generateChart(Int(characterFrequencyLength.value))
+            setCharts(getXAxisLabel(), values: getXAxisData(getXAxisLabel()))
+        }
+    }
+   
+    @IBAction func changeSegment(sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            chartView.hidden = false
+            characterFrequencyLength.hidden = false
+            informationTextView.hidden=true
+            
+        case 1:
+            chartView.hidden = true
+            characterFrequencyLength.hidden = true
+            informationTextView.hidden=false
+            informationTextView.text = getStaticalInformation()
+        default:
+            break;
+        }
+    }
+    
+    
+    func setCharts(xAxisLabels: [String], values: [Double])
+    {
+        
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<xAxisLabels.count
+        {
+            dataEntries.append(BarChartDataEntry(value: values[i], xIndex: i))
+            
+        }
+        
+        let charDataSet = BarChartDataSet(yVals: dataEntries, label: "Character Frequency")
+        charDataSet.valueFormatter?.minimumFractionDigits = 0
+        charDataSet.valueFormatter?.generatesDecimalNumbers=false
+        let charData = BarChartData(xVals: xAxisLabels, dataSet: charDataSet)
+        chartView.data=charData
+        
     }
 
 
