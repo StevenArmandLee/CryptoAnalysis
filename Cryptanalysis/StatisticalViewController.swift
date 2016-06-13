d8bc400fe556ce58a1cce996afc35602febdb6c6
steven lee <stevenlee@stevens-mac.local>
2016-06-13 12:36:39 +0800

    changed the UI of StatisticalViewController, the initial view we put two view in one view by hiding it and showing it, however it is hard to control them, therefore I made two separated view inside a view.

--- Cryptanalysis/StatisticalViewController.swift
+++ Cryptanalysis/StatisticalViewController.swift
@@ -12,22 +12,13 @@ import Charts
 class StatisticalViewController: UIViewController {
 
     
-    @IBOutlet weak var chartView: BarChartView!
+    @IBOutlet weak var graphViewContainer: UIView!
     
-    @IBOutlet weak var informationTextView: UITextView!
-    @IBOutlet weak var characterFrequencyLength: UISlider!
+   
     @IBOutlet weak var segmentedControl: UISegmentedControl!
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        chartView.noDataText = "There is no data being provided"
-        chartView.descriptionText = ""
-        chartView.animate(xAxisDuration: 2, yAxisDuration: 2)
-        chartView.doubleTapToZoomEnabled = false
-        generateChart(Int(characterFrequencyLength.value))
-        setCharts(getXAxisLabel(), values: getXAxisData(getXAxisLabel()))
-        informationTextView.text = getStaticalInformation()
         
     }
 
@@ -39,61 +30,24 @@ class StatisticalViewController: UIViewController {
     override func viewWillAppear(animated: Bool)
     {
         
-        generateChart(Int(characterFrequencyLength.value))
-        setCharts(getXAxisLabel(), values: getXAxisData(getXAxisLabel()))
-        informationTextView.text = getStaticalInformation()
+        
+        //informationTextView.text = getStaticalInformation()
     }
     
-    @IBAction func characterFrequencyDidChange(sender: UISlider) {
-        
-        sender.setValue(Float(lround(Double(characterFrequencyLength.value))), animated: true)
-        
-        if(Int(sender.value) <= trimmedText.characters.count)
-        {
-            generateChart(Int(characterFrequencyLength.value))
-            setCharts(getXAxisLabel(), values: getXAxisData(getXAxisLabel()))
-        }
-    }
-   
+    
     @IBAction func changeSegment(sender: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex
         {
         case 0:
-            chartView.hidden = false
-            characterFrequencyLength.hidden = false
-            informationTextView.hidden=true
+            graphViewContainer.hidden=false
             
         case 1:
-            chartView.hidden = true
-            characterFrequencyLength.hidden = true
-            informationTextView.hidden=false
-            informationTextView.text = getStaticalInformation()
+            graphViewContainer.hidden=true
         default:
             break;
         }
     }
     
-    
-    func setCharts(xAxisLabels: [String], values: [Double])
-    {
-        
-        
-        var dataEntries: [BarChartDataEntry] = []
-        
-        for i in 0..<xAxisLabels.count
-        {
-            dataEntries.append(BarChartDataEntry(value: values[i], xIndex: i))
-            
-        }
-        
-        let charDataSet = BarChartDataSet(yVals: dataEntries, label: "Character Frequency")
-        charDataSet.valueFormatter?.minimumFractionDigits = 0
-        charDataSet.valueFormatter?.generatesDecimalNumbers=false
-        let charData = BarChartData(xVals: xAxisLabels, dataSet: charDataSet)
-        chartView.data=charData
-        
-    }
-
 
 }
 
