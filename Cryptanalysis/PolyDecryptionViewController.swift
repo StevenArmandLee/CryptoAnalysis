c8c027537e1e62441601d58f52793748e2e2b3f0
branata <branatakurniawan_9c@yahoo.com>
2016-06-16 23:22:26 +0800

    Edited PolyDecryptionViewController.swift
    modified on 16 june 2016

--- Cryptanalysis/PolyDecryptionViewController.swift
+++ Cryptanalysis/PolyDecryptionViewController.swift
@@ -14,6 +14,7 @@ class PolyDecryptionController: UIViewController
     
     
     @IBOutlet weak var resultTextView: UITextView!
+    @IBOutlet weak var keyField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,5 +32,50 @@ class PolyDecryptionController: UIViewController
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func buttonPressed(sender: UIButton) {
+        let alphanumeric = "0123456789abcdefghijklmnopqrstuvwxyz"
+        let ctext = globalModifiedText.lowercaseString
+        let lengthOfCtext = ctext.characters.count
+        
+        let key = keyField.text!.lowercaseString
+        let lengthOfKey = key.characters.count
+        
+        var ptext = String()
+        
+        for i in 0..<lengthOfCtext {
+            let indexOfCtext = ctext.startIndex.advancedBy(i)
+            let indexOfKey = key.startIndex.advancedBy(i%lengthOfKey)
+            
+            let ctextNum = findCharNum(ctext[indexOfCtext])
+            
+            if ctextNum == -1 {
+                ptext.append(ctext[indexOfCtext])
+            }
+            else{
+                let keyNum = findCharNum(key[indexOfKey])
+                let pchar = alphanumeric.startIndex.advancedBy((ctextNum + keyNum) % 36)
+                ptext.append(alphanumeric[pchar])
+            }
+        }
+        
+        
+        resultTextView.text = ptext
+        
+    }
+    
+    func findCharNum(c : Character) -> Int
+    {
+        let alphanumeric = "0123456789abcdefghijklmnopqrstuvwxyz"
+        var index = 0
+        
+        if let idx = alphanumeric.characters.indexOf(c) {
+            index = alphanumeric.startIndex.distanceTo(idx)
+        }else {
+            // cannot find the char in the alphanumeric
+            index = -1
+        }
+        
+        return index
     }
 }
